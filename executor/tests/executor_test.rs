e65d247e27efdb17ece311ecfdda7ef0cf93279d
nuccE541 <10245101541@stu.ecnu.edu.cn>
2026-05-22 00:31:24 +0800

    test: add B command reverse state tests

--- executor/tests/executor_test.rs
+++ executor/tests/executor_test.rs
@@ -177,3 +177,67 @@ mod turn_right_tests {
         assert_eq!(expected_pose, executor.query());
     }
 }
+
+mod back_state_tests {
+    use super::*;
+
+    #[test]
+    fn should_return_x_minus_1_given_status_is_reverse_command_is_m_and_facing_is_e() {
+        // given
+        let original_pose = Pose::new(0, 0, 'E');
+        let mut executor = Executor::with_pose(original_pose);
+        
+        // when
+        executor.execute("BM");
+
+        // then
+        let expected_pose = Pose::new(-1, 0, 'E');
+        assert_eq!(expected_pose, executor.query());
+
+    }
+
+    #[test]
+    fn should_return_y_plus_1_given_command_is_bbm_and_facing_is_n() {
+        // given
+        let original_pose = Pose::new(0, 0, 'N');
+        let mut executor = Executor::with_pose(original_pose);
+        
+        // when
+        executor.execute("BBM");
+
+        // then
+        let expected_pose = Pose::new(0, 1, 'N');
+        assert_eq!(expected_pose, executor.query());
+
+    }
+
+    #[test]
+    fn should_return_s_given_status_is_reverse_command_is_l_and_facing_is_e() {
+        // given
+        let original_pose = Pose::new(0, 0, 'E');
+        let mut executor = Executor::with_pose(original_pose);
+        
+        // when
+        executor.execute("L");
+
+        // then
+        let expected_pose = Pose::new(0, 0, 'S');
+        assert_eq!(expected_pose, executor.query());
+
+    }
+    
+    #[test]
+    fn should_return_n_given_status_is_reverse_command_is_r_and_facing_is_e() {
+        // given
+        let original_pose = Pose::new(0, 0, 'E');
+        let mut executor = Executor::with_pose(original_pose);
+        
+        // when
+        executor.execute("L");
+
+        // then
+        let expected_pose = Pose::new(0, 0, 'n');
+        assert_eq!(expected_pose, executor.query());
+
+    }
+}
