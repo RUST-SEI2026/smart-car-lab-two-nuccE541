118ff3ee8d6a5f33269947f550957aee5f23d760
nuccE541 <10245101541@stu.ecnu.edu.cn>
2026-05-22 02:05:52 +0800

    test: update back and fast state test cases

--- executor/tests/executor_test.rs
+++ executor/tests/executor_test.rs
@@ -241,3 +241,119 @@ mod back_state_tests {
 
     }
 }
+
+mod fast_state_tests {
+    use super::*;
+
+    #[test]
+    fn should_return_x_plus_2_given_status_is_fast_command_is_m_and_facing_is_e() {
+        // given
+        let original_pose = Pose::new(0, 0, 'E');
+        let mut executor = Executor::with_pose(original_pose);
+        
+        // when
+        executor.execute("FM");
+
+        // then
+        let expected_pose = Pose::new(2, 0, 'E');
+        assert_eq!(expected_pose, executor.query());
+
+    }
+
+    #[test]
+    fn should_return_x_plus_1_and_n_given_status_is_fast_command_is_l_and_facing_is_e() {
+        // given
+        let original_pose = Pose::new(0, 0, 'E');
+        let mut executor = Executor::with_pose(original_pose);
+        
+        // when
+        executor.execute("FL");
+
+        // then
+        let expected_pose = Pose::new(1, 0, 'N');
+        assert_eq!(expected_pose, executor.query());
+
+    }
+
+    #[test]
+    fn should_return_x_plus_1_and_s_given_status_is_fast_command_is_r_and_facing_is_e() {
+        // given
+        let original_pose = Pose::new(0, 0, 'E');
+        let mut executor = Executor::with_pose(original_pose);
+        
+        // when
+        executor.execute("FR");
+
+        // then
+        let expected_pose = Pose::new(1, 0, 'S');
+        assert_eq!(expected_pose, executor.query());
+
+    }
+
+    #[test]
+    fn should_return_x_plus_1_given_command_is_ffm_and_facing_is_e() {
+        // given
+        let original_pose = Pose::new(0, 0, 'E');
+        let mut executor = Executor::with_pose(original_pose);
+        
+        // when
+        executor.execute("FFM");
+
+        // then
+        let expected_pose = Pose::new(1, 0, 'E');
+        assert_eq!(expected_pose, executor.query());
+
+    }
+    
+}
+
+
+mod back_and_fast_state_tests {
+    use super::*;
+
+    #[test]
+    fn should_return_x_minus_2_given_status_is_reverse_and_fast_command_is_m_and_facing_is_e() {
+        // given
+        let original_pose = Pose::new(0, 0, 'E');
+        let mut executor = Executor::with_pose(original_pose);
+        
+        // when
+        executor.execute("BFM");
+
+        // then
+        let expected_pose = Pose::new(-2, 0, 'E');
+        assert_eq!(expected_pose, executor.query());
+
+    }
+
+    #[test]
+    fn should_return_x_minus_1_and_s_given_status_is_reverse_and_fast_command_is_l_and_facing_is_e() {
+        // given
+        let original_pose = Pose::new(0, 0, 'E');
+        let mut executor = Executor::with_pose(original_pose);
+        
+        // when
+        executor.execute("BFL");
+
+        // then
+        let expected_pose = Pose::new(-1, 0, 'S');
+        assert_eq!(expected_pose, executor.query());
+
+    }
+
+    #[test]
+    fn should_return_x_minus_1_and_n_given_status_is_reverse_and_fast_command_is_r_and_facing_is_e() {
+        // given
+        let original_pose = Pose::new(0, 0, 'E');
+        let mut executor = Executor::with_pose(original_pose);
+        
+        // when
+        executor.execute("BFR");
+
+        // then
+        let expected_pose = Pose::new(-1, 0, 'N');
+        assert_eq!(expected_pose, executor.query());
+
+    }
+    
+}
